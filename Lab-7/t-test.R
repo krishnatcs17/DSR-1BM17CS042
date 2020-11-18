521d8fb98e8e7618b3b6c162b02d4a4f6ef4a32f
krishnatcs17 <krishnat.cs17@bmsce.ac.in>
2020-11-18 15:21:43 +0530

    2 sample t-test

--- Lab-7/t-test.R
+++ Lab-7/t-test.R
@@ -1,11 +1,14 @@
-library(BSDA)
+# library(BSDA)
 library(visualize)
 
-Machine1 <- c(363, 404, 518, 521, 613, 587, 412, 469, 468)
+Machine1 <- c(363, 404, 518, 521, 613, 587, 412, 469, 468, 406)
 Machine2 <- c(536, 474, 556, 549, 479, 422, 414, 505, 505, 552)
 
-mean(Machine1)
-mean(Machine2)
+sd(Machine1)
+sd(Machine2)
 
-t.test(Machine1, mu=50, conf.level = 0.95)
-t.test(Machine2, mu=50, conf.level = 0.95)
+?t.test
+t.test(x = Machine1, y = Machine2,  var.equal = T, conf.level = 0.95)
+
+?visualize.t
+visualize.t(stat=c(-0.74677, 0.74677),df=18,section="tails")
